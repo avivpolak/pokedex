9754b44f3c4b6e8ae89ece0a1035b5153df029ae
[aviv polak] <[avivp0437@gmail.com]>
2021-10-24 15:24:57 +0300

    username

--- src/index.js
+++ src/index.js
@@ -6,144 +6,157 @@ document.getElementById("img").addEventListener("mouseenter", handleImgEnter);
 document.getElementById("img").addEventListener("mouseleave", handleImgLeave);
 const baseURL = "https://pokeapi.co/api/v2/pokemon/";
 const getChar = async (baseURL, name = "") => {
-  try {
-    const response = await axios.get(baseURL + name);
-    const json = response.data;
-    return json;
-  } catch (error) {
-    return undefined;
-  }
+    try {
+        const response = await axios.get(baseURL + name);
+        const json = response.data;
+        return json;
+    } catch (error) {
+        return undefined;
+    }
 };
 async function manageDisplayResult(searchName) {
-  cleanBoard();
-  result = await getChar(baseURL, searchName + "/"); // because the api gets only lower cased name
-  if (!result) {
-    data = {};
-    displayResult(undefined);
-    return;
-  } else {
-    data = {
-      height: result.height,
-      weight: result.weight,
-      pokemonName: result.name,
-      frontImg: result.sprites["front_default"],
-      backImg: result.sprites["back_default"],
-      types: result.types,
-    };
-    displayResult(data);
-  }
+    cleanBoard();
+    result = await getChar(baseURL, searchName + "/"); // because the api gets only lower cased name
+    if (!result) {
+        data = {};
+        displayResult(undefined);
+        return;
+    } else {
+        data = {
+            height: result.height,
+            weight: result.weight,
+            pokemonName: result.name,
+            frontImg: result.sprites["front_default"],
+            backImg: result.sprites["back_default"],
+            types: result.types,
+        };
+        displayResult(data);
+    }
 }
 function handleSearch() {
-  displayThisPokemon(document.getElementById("name").value.toLowerCase());
+    displayThisPokemon(document.getElementById("name").value.toLowerCase());
 }
 function displayThisPokemon(name) {
-  manageDisplayResult(name.toLowerCase());
+    manageDisplayResult(name.toLowerCase());
 }
 
 function displayResult(data) {
-  if (!data) {
-    document.getElementById("error").innerText = "No such pokimon exsist!";
-    return;
-  }
-  showInfo();
+    if (!data) {
+        document.getElementById("error").innerText = "No such pokimon exsist!";
+        return;
+    }
+    showInfo();
 }
 function showInfo() {
-  document.getElementById("types").append("Types: ");
-  for (let line of data.types) {
-    let typeElem = createElement("button", [line.type.name]);
-    typeElem.addEventListener("click", handleshowThisType);
-    document.getElementById("types").append(typeElem);
-  }
-  document.getElementById("pokemonName").innerText += "Name: " + data["pokemonName"];
-  document.getElementById("height").innerText += "Height: " + data["height"];
-  document.getElementById("weight").innerText += "weight: " + data["weight"];
-  document.getElementById("img").setAttribute("src", data.frontImg);
+    document.getElementById("types").append("Types: ");
+    for (let line of data.types) {
+        let typeElem = createElement("button", [line.type.name]);
+        typeElem.addEventListener("click", handleshowThisType);
+        document.getElementById("types").append(typeElem);
+    }
+    document.getElementById("pokemonName").innerText +=
+        "Name: " + data["pokemonName"];
+    document.getElementById("height").innerText += "Height: " + data["height"];
+    document.getElementById("weight").innerText += "weight: " + data["weight"];
+    document.getElementById("img").setAttribute("src", data.frontImg);
 }
 
 function handleImgEnter() {
-  document.getElementById("img").setAttribute("src", data.backImg);
+    document.getElementById("img").setAttribute("src", data.backImg);
 }
 function handleImgLeave() {
-  document.getElementById("img").setAttribute("src", data.frontImg);
+    document.getElementById("img").setAttribute("src", data.frontImg);
 }
 function handleshowThisType(e) {
-  document.getElementById("sameType").innerText = "";
-  showThisType(e.target.innerText);
+    document.getElementById("sameType").innerText = "";
+    showThisType(e.target.innerText);
 }
 async function ListFromType(result, type) {
-  let list = [];
-  for (let pokemon of result.results) {
-    getChar(pokemon.url, "").then((result) => {
-      let listOfTypes = [];
-      for (let Type in result.types) {
-        listOfTypes.push(result.types[Type].type.name);
-      }
-      if (listOfTypes.includes(type)) list.push(result.name);
-    });
-  }
+    let list = [];
+    for (let pokemon of result.results) {
+        getChar(pokemon.url, "").then((result) => {
+            let listOfTypes = [];
+            for (let Type in result.types) {
+                listOfTypes.push(result.types[Type].type.name);
+            }
+            if (listOfTypes.includes(type)) list.push(result.name);
+        });
+    }
 
-  return list;
+    return list;
 }
 async function showThisType(type) {
-  let list = await displayThisType(type);
-  displayThisType(type).then(() => {
-    for (let pokemon of list) {
-      let typeElem = createElement("div", [pokemon]);
-      typeElem.addEventListener("click", handleDisplayFromList);
-      document.getElementById("sameType").append(typeElem);
-    }
-  });
+    let list = await displayThisType(type);
+    displayThisType(type).then(() => {
+        for (let pokemon of list) {
+            let typeElem = createElement("div", [pokemon]);
+            typeElem.addEventListener("click", handleDisplayFromList);
+            document.getElementById("sameType").append(typeElem);
+        }
+    });
 }
 
 function handleDisplayFromList(e) {
-  displayThisPokemon(e.target.innerText);
+    displayThisPokemon(e.target.innerText);
 }
 async function displayThisType(type) {
-  return await ListFromType(await getChar(baseURL + "?limit=1000"), type);
+    return await ListFromType(await getChar(baseURL + "?limit=1000"), type);
 }
 function cleanBoard() {
-  document.getElementById("name").value = "";
-  document.getElementById("types").innerHTML = "";
-  document.getElementById("pokemonName").innerText = "";
-  document.getElementById("height").innerText = "";
-  document.getElementById("weight").innerText = "";
-  document.getElementById("img").setAttribute("src", "");
-  document.getElementById("error").innerText = "";
-  document.getElementById("sameType").innerText = "";
+    document.getElementById("name").value = "";
+    document.getElementById("types").innerHTML = "";
+    document.getElementById("pokemonName").innerText = "";
+    document.getElementById("height").innerText = "";
+    document.getElementById("weight").innerText = "";
+    document.getElementById("img").setAttribute("src", "");
+    document.getElementById("error").innerText = "";
+    document.getElementById("sameType").innerText = "";
 }
 
-function createElement(tagname, children = [], classes = [], attributes, events) {
-  //the most generic element builder.
-  //we will build all the elements here.
+function createElement(
+    tagname,
+    children = [],
+    classes = [],
+    attributes,
+    events
+) {
+    //the most generic element builder.
+    //we will build all the elements here.
 
-  const el = document.createElement(tagname);
+    const el = document.createElement(tagname);
 
-  //children
+    //children
 
-  for (let child of children) {
-    if (typeof child === "string" || typeof child === "number") {
-      child = document.createTextNode(child);
+    for (let child of children) {
+        if (typeof child === "string" || typeof child === "number") {
+            child = document.createTextNode(child);
+        }
+        el.appendChild(child);
     }
-    el.appendChild(child);
-  }
 
-  //classes
+    //classes
 
-  for (const cls of classes) {
-    el.classList.add(cls);
-  }
+    for (const cls of classes) {
+        el.classList.add(cls);
+    }
 
-  //attrubutes
+    //attrubutes
 
-  for (const attr in attributes) {
-    el.setAttribute(attr, attributes[attr]);
-  }
+    for (const attr in attributes) {
+        el.setAttribute(attr, attributes[attr]);
+    }
 
-  //attrubutes
+    //attrubutes
 
-  for (const event in events) {
-    el.addEventListener(event, events[event]);
-  }
+    for (const event in events) {
+        el.addEventListener(event, events[event]);
+    }
 
-  return el;
+    return el;
+}
+document.getElementById("login").addEventListener("click", login);
+function login() {
+    const username = document.getElementById("usernameInput").value;
+    localStorage["username"] = username;
+    document.getElementById("username").innerText = username;
 }
